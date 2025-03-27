22069df0e3f4ca16f1dd975a1af60a0c1d3418ed
Emagmunioz <emagmunioz@gmail.com>
2025-03-27 09:37:22 +0100

    modify Vehicle class to handle axles for trucks

--- src/main/java/dev/peaje/Vehicle.java
+++ src/main/java/dev/peaje/Vehicle.java
@@ -3,11 +3,18 @@ package dev.peaje;
 public class Vehicle {
     private final String licensePlate;
     private final VehicleType type;
+    private final int axles;
 
     public Vehicle(String licensePlate, VehicleType type) {
+        this(licensePlate, type, 0);
+    }
+
+    public Vehicle(String licensePlate, VehicleType type, int axles) {
         this.licensePlate = licensePlate;
         this.type = type;
+        this.axles = axles;
     }
+
 
     public String getLicensePlate() {
         return licensePlate;
@@ -15,6 +22,10 @@ public class Vehicle {
 
     public VehicleType getType() {
         return type;
+    }
+
+    public int getAxles() {
+        return axles;
     }
     
 
